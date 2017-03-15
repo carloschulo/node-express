5bf079d1c6549d7091d89bd3dbdc9672e922b5e7
Carlos Henriquez <carloschulo@gmail.com>
2017-03-14 23:03:03 -0400

    added views

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', function(req, res){
   res.send(`
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <h1>Welcome</h1>
+    <h1>Welcome HOME</h1>
     <img src="/images/misc/background.jpg" alt="" style="height: 200px;">
     <p>This is the homepage</p>
   `);
